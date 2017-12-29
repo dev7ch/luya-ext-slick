0330e2bdce7e04b9245a4fd805014ba93aaad0e7
Silvan <silvan@dev7.ch>
2017-12-29 19:35:56 +0100

    update php

--- src/SlickWidget.php
+++ src/SlickWidget.php
@@ -22,7 +22,7 @@ class SlickWidget extends \luya\base\Widget
         SlickAsset::register($this->getView());
         ResourcesAsset::register($this->getView());
 
-        if (Module::slickConfig() != false && !array_key_exists( 'slickConfigWidget', $this->view->params)) {
+        if (Module::slickConfig() != false) {
 
             $this->slickConfig = Module::slickConfig();
 
