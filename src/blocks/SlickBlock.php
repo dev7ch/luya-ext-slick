6e55d9248229cabdee7a3b8e899fcf1d7b64a910
Silvan <silvan@dev7.ch>
2018-01-01 20:25:40 +0100

    add inheritdoc

--- src/blocks/SlickBlock.php
+++ src/blocks/SlickBlock.php
@@ -24,7 +24,7 @@ class SlickBlock extends BaseSlickBlock
     public $cacheExpiration = 3600;
 
     /**
-     * @return string|\luya\cms\frontend\blockgroups\ProjectGroup
+     * {@inheritdoc}
      */
     public function blockGroup()
     {
